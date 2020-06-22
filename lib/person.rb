b08d3183c1a8eb50666f43422b709beacb927839
travis Lawson <travislawclimb@gmail.com>
2020-06-22 18:25:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,42 @@
-class Person
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
   
-  def name=(person_name)
-    @name = person_name 
-  end   
+#   def name=(person_name)
+#     @name = person_name 
+#   end   
   
-  def name
-    @name 
-  end   
+#   def name
+#     @name 
+#   end   
   
-  def job=(person_job)
-    @job = person_job
-  end 
+#   def job=(person_job)
+#     @job = person_job
+#   end 
   
-  def job
-    @job 
-  end   
+#   def job
+#     @job 
+#   end   
   
-end 
+# end 
 
 
